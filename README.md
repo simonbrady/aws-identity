8981f13ec526b9940ad4688ac6fe6a5cebb39cb6
Simon Brady <Simon.Brady@concepts.co.nz>
2019-01-21 12:26:32 +1300

    Minimal working implementation

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # aws-identity
-Command-line tool to manage STS credentials
+
+A simple command-line tool to manage
+[temporary STS credentials](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_temp.html), written using the
+[AWS SDK for Go](https://aws.amazon.com/sdk-for-go/). When run without options it will use
+[GetCallerIdentity](https://docs.aws.amazon.com/STS/latest/APIReference/API_GetCallerIdentity.html)
+to display the current user identity. It can also get temporary credentials and inject them into a new shell process
+as [CLI environment variables](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html).
+
+To see a full list of options, use the `-h` or `--help` command-line option.
+
+## Assume a named role in the same account
+
+Get the account for the current user identity (which could be set through a
+[named profile](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html))
+then use [AssumeRole](https://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html)
+to assume a named role in that account. Takes an optional
+[MFA token code](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_mfa.html) if the trust condition for
+assuming the role requires MFA.
+
+```
+aws-identity -r <role-name> [-t token-code]
+```
+
+e.g.
+
+```
+$ aws-identity -r admin -t 123456
+Spawning subshell for role arn:aws:iam::111122223333:role/admin
+```
+
+### Assume a named cross-account role
+
+As above but takes the target account number to assume the role in.
+
+```
+aws-identity -a <account> -r role-name [-t token-code]
+```
+
+e.g.
+
+```
+$ aws-identity -a 444455556666 -r admin -t 234567
+Spawning subshell for role arn:aws:iam::444455556666:role/admin
+```
+
+### Authenticate the current user with MFA
+
+Rather than assuming a new role identity, use
+[GetSessionToken](https://docs.aws.amazon.com/STS/latest/APIReference/API_GetSessionToken.html)
+to generate temporary credentials for the current identity but with MFA.
+
+```
+aws-identity -t <token-code>
+```
+
+e.g.
+
+```
+$ aws-identity -t 345678
+Spawning subshell for user arn:aws:iam::111122223333:user/jrh
+```
+
+This is useful for tools like the [Terraform AWS provider](https://www.terraform.io/docs/providers/aws/)
+that can assume roles but don't prompt for an MFA token.
---
+++ main.go
@@ -0,0 +1,163 @@
+// Command-line tool to manage STS credentials
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+// Command-line options
+var account string
+var debug bool
+var duration int64
+var mfaSerial string
+var mfaToken string
+var quiet bool
+var role string
+var sessionName string
+
+func init() {
+	boolOpts := []struct {
+		variable  *bool
+		shortName string
+		longName  string
+		usage     string
+	}{
+		{&debug, "D", "debug", "Enable debugging output"},
+		{&quiet, "q", "quiet", "Minimise output"},
+	}
+	for _, opt := range boolOpts {
+		flag.BoolVar(opt.variable, opt.shortName, false, opt.usage)
+		flag.BoolVar(opt.variable, opt.longName, false, opt.usage)
+	}
+
+	int64Opts := []struct {
+		variable     *int64
+		shortName    string
+		longName     string
+		defaultValue int64
+		usage        string
+	}{
+		{&duration, "d", "duration", 3600, "Lifetime of temporary credentials in seconds"},
+	}
+	for _, opt := range int64Opts {
+		flag.Int64Var(opt.variable, opt.shortName, opt.defaultValue, opt.usage)
+		flag.Int64Var(opt.variable, opt.longName, opt.defaultValue, opt.usage)
+	}
+
+	stringOpts := []struct {
+		variable  *string
+		shortName string
+		longName  string
+		usage     string
+	}{
+		{&account, "a", "account", "Target account, default derived from caller identity"},
+		{&mfaSerial, "m", "mfa-serial", "ARN of MFA device, default derived from caller identity"},
+		{&mfaToken, "t", "mfa-token", "MFA token code"},
+		{&role, "r", "role", "Target role name"},
+		{&sessionName, "n", "session-name", "Session name, default derived from caller identity"},
+	}
+	for _, opt := range stringOpts {
+		flag.StringVar(opt.variable, opt.shortName, "", opt.usage)
+		flag.StringVar(opt.variable, opt.longName, "", opt.usage)
+	}
+
+	flag.Parse()
+}
+
+// newClient creates an STS client, with session-level debugging if requested on the command line.
+func newClient() *sts.STS {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	config := aws.NewConfig()
+	if debug {
+		config.WithLogLevel(aws.LogDebugWithHTTPBody)
+	}
+	return sts.New(sess, config)
+}
+
+// getRoleCreds returns STS credentials from assuming the role given in roleArn.
+func getRoleCreds(client *sts.STS, roleArn string) *sts.Credentials {
+	input := new(sts.AssumeRoleInput).
+		SetDurationSeconds(duration).
+		SetRoleArn(roleArn).
+		SetRoleSessionName(sessionName)
+	if mfaToken != "" {
+		input.SetSerialNumber(mfaSerial).SetTokenCode(mfaToken)
+	}
+	result, err := client.AssumeRole(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result.Credentials
+}
+
+// getMFACreds returns STS credentials for the current user but in an MFA session.
+func getMFACreds(client *sts.STS) *sts.Credentials {
+	input := new(sts.GetSessionTokenInput).
+		SetDurationSeconds(duration).
+		SetSerialNumber(mfaSerial).
+		SetTokenCode(mfaToken)
+	result, err := client.GetSessionToken(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result.Credentials
+}
+
+// spawnSubShell launches a shell for the named principal, injecting the given credentials.
+func spawnSubShell(principal string, creds *sts.Credentials) {
+	if !quiet {
+		fmt.Printf("Spawning subshell for %s\n", principal)
+	}
+	cmd := exec.Command("bash", "-i")
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	cmd.Env = append(os.Environ(),
+		"AWS_ACCESS_KEY_ID="+*creds.AccessKeyId,
+		"AWS_SECRET_ACCESS_KEY="+*creds.SecretAccessKey,
+		"AWS_SESSION_TOKEN="+*creds.SessionToken)
+	cmd.Run()
+}
+
+func main() {
+	log.SetPrefix("aws-get-creds: ")
+	log.SetFlags(0)
+	client := newClient()
+
+	// Infer any missing options from the current user's identity
+	identity, err := client.GetCallerIdentity(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	arn := strings.Split(*identity.Arn, ":")
+	username := strings.Split(arn[5], "/")[1]
+	if account == "" {
+		account = arn[4]
+	}
+	if mfaSerial == "" {
+		mfaSerial = fmt.Sprintf("arn:aws:iam::%s:mfa/%s", arn[4], username)
+	}
+	if sessionName == "" {
+		sessionName = username
+	}
+
+	// Choose operation to perform based on which options were supplied
+	switch {
+	case role != "":
+		roleArn := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, role)
+		spawnSubShell("role "+roleArn, getRoleCreds(client, roleArn))
+	case mfaToken != "":
+		spawnSubShell("user "+*identity.Arn, getMFACreds(client))
+	default:
+		fmt.Println(*identity.Arn)
+	}
+}
